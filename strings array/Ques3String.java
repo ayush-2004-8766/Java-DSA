7944d235962274e0fcde2ce409cad83b7a1c4201
Ayush Mishra <am3836497@gmail.com>
2026-06-19 21:50:41 +0530

    string practice

--- strings array/Ques3String.java
+++ strings array/Ques3String.java
@@ -1,6 +1,13 @@
+// convert integwer to string ?
+import java.util.Scanner;
 public class Ques3String {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter any Number :");
+        int n = sc.nextInt();
+        String s = Integer.toString(n);
+        System.out.println("your enter string :"+s);
+        sc.close();
     }
     
 }
