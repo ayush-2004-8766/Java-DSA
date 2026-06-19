8b82c4000a565ae73a8386795dcc218fa535de5f
Ayush Mishra <am3836497@gmail.com>
2026-06-19 23:08:01 +0530

    sorting practice

--- strings array/Question4String.java
+++ strings array/Question4String.java
@@ -1,11 +1,12 @@
 // printing all substring of a string?
 public class Question4String {
     public static void main(String[] args) {
-        String str = "ayush";
+        String str = "Ayush";
         for(int i =0;i<str.length();i++){
             for(int j=i+1;j<=str.length();j++){
-                System.out.println(str.substring(i,j));
+                System.out.print(str.substring(i,j)+" ");
             }
+            System.out.println();
         }
     }
 }
