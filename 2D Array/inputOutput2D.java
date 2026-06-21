215d6540be021ce065231d092c13fb4bc06fbd00
Ayush Mishra <am3836497@gmail.com>
2026-06-21 15:50:06 +0530

    2D array practice

---
+++ 2D Array/inputOutput2D.java
@@ -0,0 +1,14 @@
+//Taking input and output in array?
+
+public class inputOutput2D {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][4];
+        for(int i = 0; i<3;i++){
+            for(int j = 0; j<4;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+      }
+    
+}
