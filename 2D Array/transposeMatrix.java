258eba61e2c6278e5df5a84270987c70896a2837
Ayush Mishra <am3836497@gmail.com>
2026-06-23 23:17:57 +0530

    2D array practice

--- 2D Array/transposeMatrix.java
+++ 2D Array/transposeMatrix.java
@@ -10,7 +10,7 @@ public class transposeMatrix {
         
     }
     public static void main(String[] args) {
-        int arr[][] = {{2,3,6,5},{4,5,6,9},{7,8,9,6},{2,3,6,9}};
+        int arr[][] = {{2,3,6,5,8},{4,5,6,9,7},{7,8,9,6,5},{2,3,6,9,4}};
         print(arr);
         //transpose
         for(int i =0; i<arr.length;i++){
