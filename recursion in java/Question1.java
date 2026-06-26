82849f32a0212f6a39bb33423429502e6a9fd634
Ayush Mishra <am3836497@gmail.com>
2026-06-26 22:06:59 +0530

    recursion practice

---
+++ recursion in java/Question1.java
@@ -0,0 +1,12 @@
+public class Question1 {
+    public static void main(String[] args) {
+        ajay(1);
+    }
+    public static void ajay(int n) {
+        if(n==5)return;
+        System.out.println("ayush");
+        ajay(n+1);
+        
+    }
+    
+}
